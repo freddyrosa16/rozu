938d3636de2bcb3ec304814b3fd2cf407596f130
freddyrosa16 <321212545+freddyrosa16@users.noreply.github.com>
2026-09-09 18:56:17 -0400

    Add initial Python agent backend

---
+++ backend/main.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from openai import OpenAI
+
+def main():
+
+    # To load enviroment variables
+    load_dotenv()
+    api_key = os.environ.get('OPENROUTER_API_KEY')
+    if api_key == None:
+        raise RuntimeError('API key nor found please write your OpenRouter API key in .env')
+
+    # Handles command line argument
+    parser = argparse.ArgumentParser(description="Rozu AI Agent Bot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    # Creating a client
+    client = OpenAI(
+        base_url='https://openrouter.ai/api/v1',
+        api_key=api_key,
+    )
+
+    # Get a response from the model
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": args.user_prompt,
+            }
+        ],
+    )
+    print(f'Prompt tokens: {response.usage.prompt_tokens}')
+    print(f'Response tokens: {response.usage.completion_tokens}')
+    print(response.choices[0].message.content)
+
+if __name__ == "__main__":
+    main()
